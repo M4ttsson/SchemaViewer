1b993fdceb9e84ffaf6cd23255e7cc4c33aa1683
M4ttsson <timothy.mattsson@gmail.com>
2017-11-07 22:08:38 +0100

    Add method to get ical from the provided url

--- SchemaViewer/Controllers/CalendarController.cs
+++ SchemaViewer/Controllers/CalendarController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SchemaViewer.Models;
+using System.IO;
+using System.Net;
+using Ical.Net;
 
 namespace SchemaViewer.Controllers
 {
@@ -11,18 +14,47 @@ namespace SchemaViewer.Controllers
     public class CalendarController : Controller
     {
 		[HttpGet("[action]")] 
-		public IEnumerable<CalendarEvent> GetCalendarEvents(string url)
+		public IEnumerable<Models.CalendarEvent> GetCalendarEvents(string url)
 		{
-			return new List<CalendarEvent>()
+			// TODO: Error handling...
+			var calendarCollection = Calendar.LoadFromStream(GetIcalStream(url));
+			var firstCalendar = calendarCollection.First();
+
+			List<Models.CalendarEvent> events = new List<Models.CalendarEvent>();
+
+			foreach (var ev in firstCalendar.Events)
 			{
-				new CalendarEvent()
+				events.Add(new Models.CalendarEvent()
 				{
-					Start = DateTime.Now.AddDays(1).ToString("yy-MM-dd HH:mm"),
-					End = DateTime.Now.AddDays(1).AddHours(4).ToString("yy-MM-dd HH:mm"),
-					Location = "B089",
-					Summary = "Teacher, course name"
-				}
-			};
+					Start = ev.Start.ToString(),
+					End = ev.End.ToString(),
+					Location = ev.Location,
+					Summary = ev.Summary
+				});
+			}
+			return events;
+			//return new List<Models.CalendarEvent>()
+			//{
+			//	new Models.CalendarEvent()
+			//	{
+			//		Start = DateTime.Now.AddDays(1).ToString("yy-MM-dd HH:mm"),
+			//		End = DateTime.Now.AddDays(1).AddHours(4).ToString("yy-MM-dd HH:mm"),
+			//		Location = "B089",
+			//		Summary = "Teacher, course name"
+			//	}
+			//};
 		}
-    }
+
+		private MemoryStream GetIcalStream(string url)
+		{
+			byte[] data = null;
+
+			using (var wc = new WebClient())
+			{
+				data = wc.DownloadData(url);
+			}
+
+			return new MemoryStream(data);
+		}
+	}
 }
